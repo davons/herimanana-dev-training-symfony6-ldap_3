4d89b64275636ad140d299f8ff190869130cddc5
Yvon Rakotonanahary <yvon.rakotonanahary@gmail.com>
2024-06-02 09:55:06 +0200

    - Connexion form login

---
+++ src/Command/CreateUserCommand.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'app:add-user',
+    description: 'Add a short description for your command',
+)]
+class CreateUserCommand extends Command
+{
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly UserPasswordHasherInterface $passwordHasher
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('email', InputArgument::OPTIONAL, 'Email')
+            ->addArgument('password', InputArgument::OPTIONAL, 'Password')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $plainPassword = $input->getArgument('password');
+        $email = $input->getArgument('email');
+
+        if ($email) {
+            $io->note(sprintf('You passed an argument: %s', $email));
+        }
+
+        if ($plainPassword) {
+            $io->note(sprintf('You passed an argument: %s', $plainPassword));
+        }
+
+        $user = new User();
+        $user->setEmail($email);
+        $user->setRoles(['ROLE_USER']);
+
+        // See https://symfony.com/doc/5.4/security.html#registering-the-user-hashing-passwords
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $plainPassword);
+        $user->setPassword($hashedPassword);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $io->success('You have a new user!');
+
+        return Command::SUCCESS;
+    }
+}
